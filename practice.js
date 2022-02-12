5d8b76c745d67ab3d1c951d7220fe47ceb334314
seyunkim0114 <seyun0114kim@gmail.com>
2022-02-11 23:20:13 -0500

    final result

---
+++ practice.js
@@ -0,0 +1,19 @@
+var oneString = "hello";
+var twoString = "bye";
+
+var combine = oneString + "\n" + twoString;
+// console.log(oneString, twoString)
+
+// var data = {
+//     x : "3",
+//     y : 4,
+//     time : 1,
+//     keypoint : "shoulder"
+// }
+
+var temp = oneString + ", " + 3;
+
+console.log(temp);
+// var FileSaver = require('file-saver');
+// var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+// FileSaver.saveAs(blob, "hello world.txt");
--- sketch.js
+++ sketch.js
@@ -2,19 +2,26 @@ let video;
 let poseNet;
 let pose;
 let skeleton;
-let s = new WebSocket("ws://localhost:8080");
+// let s = new WebSocket("ws://localhost:8080");
+var data2Export;
+var x_scale = 700;
+var y_scale = 750;
 
 function setup() {
-    createCanvas(600, 700);
+    createCanvas(x_scale, y_scale);
     //video = createCapture(VIDEO);
     video = createVideo('snowboard.mp4', vidLoad);
-    //video.hide();
+    // video = createVideo('ballet.mp4', vidLoad);
+    // video = createVideo('Taichi clip.mp4', vidLoad);
+    // video = createVideo('Joker Cut.mp4', vidLoad);
+    // video = createVideo('joker_slow.mp4', vidLoad);
+
+
+    video.hide();
     poseNet = ml5.poseNet(video, 'multiple', modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-function posetoCSV(poses){
-    //s.send(pose)
-    }
+
 function gotPoses(poses) {
     console.log(poses);
     if (poses.length > 0) {
@@ -26,23 +33,46 @@ function modelLoaded() {
     console.log('poseNet ready');
 }
 function draw() {
-    image(video, 0 ,0);
-    if(pose){
-        for(let i = 0; i<pose.keypoints.length; i++){
+    var data;
+    image(video, 0, 0);
+    data = "";
+    if (pose) {
+        // console.log("draw function called for every pose?")
+        // data = "";
+        for (let i = 0; i < pose.keypoints.length; i++) {
+            // data = "";
+            // console.log("initial value data before loop" + data);
             let x = pose.keypoints[i].position.x;
             let y = pose.keypoints[i].position.y;
-            fill(0,255,0);
-            ellipse(x,y,10,10);
-            s.send(x);
+            fill(0, 255, 0);
+            ellipse(x, y, 10, 10);
+            if (i == pose.keypoints.length - 1) {
+                data = data + x/x_scale + ", " + y/y_scale;
+            }
+            else {
+                data = data + x/x_scale + ", " + y/y_scale + ", ";
+            }
+            // data = x + ", " + y + ", " + pose.keypoints[i].part + ", " + i;
+            // console.log("data to text : " + data);
         }
-        for(let i = 0; i<skeleton.length; i++){
+        // console.log("right ankle y value: " + pose.keypoints[16]/y_scale)
+        // console.log("testing one row " + data);
+
+        for (let i = 0; i < skeleton.length; i++) {
             let a = skeleton[i][0];
             let b = skeleton[i][1];
             strokeWeight(2);
             stroke(255);
-            line(a.position.x,a.position.y, b.position.x, b.position.y);
+            line(a.position.x, a.position.y, b.position.x, b.position.y);
         }
+        data2Export = data2Export + "\n" + data;
+
+        // console.log(data2Export);
     }
+    // else {
+    //     console.log("######### no pose ###########")
+    // }
+}
 
     // pose?.forEach(async p => { // for multiple poses
     //     let ps = p.pose;
@@ -76,7 +106,7 @@ function draw() {
     //    }
     // })
 
-}
+
 function vidLoad(){
     video.stop();
     video.loop();
